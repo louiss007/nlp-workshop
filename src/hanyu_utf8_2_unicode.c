0e58ddb8160c061b80fd6c83f38109f00df3054f
louiss007 <1986zf007@163.com>
2018-09-28 00:05:38 +0800

    add unicode2utf8 func

--- src/hanyu_utf8_2_unicode.c
+++ src/hanyu_utf8_2_unicode.c
@@ -8,6 +8,7 @@
 int is2byte(char chr);
 int enc_get_utf8_size(const unsigned char* p_input);
 int enc_utf8_to_unicode_one(const unsigned char* pInput, unsigned long *Unic);
+int enc_unicode_to_utf8_one(unsigned long unic, unsigned char *pOutput, int outSize);
 /*int main()
 {
     char chr;
@@ -25,7 +26,7 @@ int enc_utf8_to_unicode_one(const unsigned char* pInput, unsigned long *Unic);
 
 int main()
 {
-    char filename[] = "1.txt";
+    char filename[] = "The_Meaning_of_Life.txt";
     FILE *fp ;
     char str_line[1024];
     if ((fp = fopen(filename, "r")) == NULL) 
@@ -42,6 +43,22 @@ int main()
         printf("%ld\n", *unic);
     }
     free(unic);
+    char filename2[] = "chinese_punctuations_unicode.txt";
+    FILE *fp2;
+    char sstr_line[1024];
+    if ((fp2 = fopen(filename2, "r")) == NULL)
+    {
+        printf("open file failed!\n");
+        return -1;
+    }
+    while(!feof(fp2))
+    {
+        unsigned char *pOutput = malloc(6 * sizeof(char));
+        fgets(sstr_line, 1024, fp2);
+        ret = enc_unicode_to_utf8_one(atol(sstr_line), pOutput, 20);
+        printf("%d\n", *pOutput);
+        free(pOutput);
+    }
     return 0;
 }
 
@@ -66,6 +83,10 @@ int enc_get_utf8_size(const unsigned char* pInput)
     return ret;
 }
 
+/**
+ * @param pInput : utf8 string
+ * @param Unic : unicode of pInput, transfer result
+ */
 int enc_utf8_to_unicode_one(const unsigned char* pInput, unsigned long *Unic)
 {
     assert(pInput != NULL && Unic != NULL);
@@ -149,5 +170,68 @@ int enc_utf8_to_unicode_one(const unsigned char* pInput, unsigned long *Unic)
     }
 
     return utfbytes;
+}
+
+/**
+ * @param unic : unicode of input string
+ * @param pOutput : utf8 of input string unic
+ */
+int enc_unicode_to_utf8_one(unsigned long unic, unsigned char *pOutput, int outSize)
+{
+    assert(pOutput != NULL);
+    assert(outSize >= 6);
+    if ( unic <= 0x0000007F )
+    {
+        // * U-00000000 - U-0000007F:  0xxxxxxx
+        *pOutput     = (unic & 0x7F);
+        return 1;
+    }
+    else if ( unic >= 0x00000080 && unic <= 0x000007FF )
+    {
+        // * U-00000080 - U-000007FF:  110xxxxx 10xxxxxx
+        *(pOutput+1) = (unic & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 6) & 0x1F) | 0xC0;
+        return 2;
+    }
+    else if ( unic >= 0x00000800 && unic <= 0x0000FFFF )
+    {
+        // * U-00000800 - U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
+        *(pOutput+2) = (unic & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >>  6) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 12) & 0x0F) | 0xE0;
+        return 3;
+    }
+    else if ( unic >= 0x00010000 && unic <= 0x001FFFFF )
+    {
+        // * U-00010000 - U-001FFFFF:  11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+3) = (unic & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 12) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 18) & 0x07) | 0xF0;
+        return 4;
+    }
+    else if ( unic >= 0x00200000 && unic <= 0x03FFFFFF )
+    {
+        // * U-00200000 - U-03FFFFFF:  111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+4) = (unic & 0x3F) | 0x80;
+        *(pOutput+3) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >> 12) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 18) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 24) & 0x03) | 0xF8;
+        return 5;
+    }
+    else if ( unic >= 0x04000000 && unic <= 0x7FFFFFFF )
+    {
+        // * U-04000000 - U-7FFFFFFF:  1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        *(pOutput+5) = (unic & 0x3F) | 0x80;
+        *(pOutput+4) = ((unic >>  6) & 0x3F) | 0x80;
+        *(pOutput+3) = ((unic >> 12) & 0x3F) | 0x80;
+        *(pOutput+2) = ((unic >> 18) & 0x3F) | 0x80;
+        *(pOutput+1) = ((unic >> 24) & 0x3F) | 0x80;
+        *pOutput     = ((unic >> 30) & 0x01) | 0xFC;
+        return 6;
+    }
+
+    return 0;
 }
 
